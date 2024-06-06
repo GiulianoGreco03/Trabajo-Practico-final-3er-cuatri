852147ece8df57839e2e6c3fce087799aa81346c
Agustina Martín <101644042+agustinamartin@users.noreply.github.com>
2024-06-06 12:46:16 -0300

    Update App.java

--- TPFinalLabo3/src/App.java
+++ TPFinalLabo3/src/App.java
@@ -2,7 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola Mundo");
         String nombre; 
-        int numGenerico;
         System.out.println("Linea de prueba 2");
     }
 }
