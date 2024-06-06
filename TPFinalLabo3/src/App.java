fb44752c4756681e105bd3d01eb8daab0919ef87
Agustina Martín <agustinamariamartin@gmail.com>
2024-06-06 12:42:35 -0300

    Cambio linea 2

--- TPFinalLabo3/src/App.java
+++ TPFinalLabo3/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola Mundo");
-        System.out.println("Git te odio") ;
+        String nombre; 
         System.out.println("Linea de prueba 2");
     }
 }
