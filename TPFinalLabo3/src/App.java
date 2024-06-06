5583d0ea83ceef313b5ea9983afb58147da197ad
GiulianoGreco03 <giulianogreco03@gmail.com>
2024-06-06 12:37:03 -0300

    Prueba 2

--- TPFinalLabo3/src/App.java
+++ TPFinalLabo3/src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola Mundo");
         System.out.println("Git te odio") ;
+        System.out.println("Linea de prueba 2");
     }
 }
