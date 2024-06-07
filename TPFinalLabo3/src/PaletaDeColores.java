3f78c62378e7459c4861e2f9005b3fe41db64fb9
GiulianoGreco03 <giulianogreco03@gmail.com>
2024-06-07 16:04:47 -0300

    Cree la paleta de colores

---
+++ TPFinalLabo3/src/PaletaDeColores.java
@@ -0,0 +1,18 @@
+public enum PaletaDeColores {
+
+    PanelPrincipal(106,25,148),
+    Botones(133,36,103),
+    BotonPresionado(115,115,115),
+    CuadradoTexto(66, 140, 140);
+
+    
+    int R;
+    int G;
+    int B;
+
+    private PaletaDeColores(int R, int G, int B){
+        this.R = R;
+        this.G = G;
+        this.B = B;
+    }
+}
