19d2fc5296d46d3b545cb2297514e819310a1550
Lucholoren <lorenzani90@gmail.com>
2024-06-15 13:47:32 -0300

    Cree usuario y EnumJuegos

---
+++ TPFinalLabo3/src/Juegos/EnumJuegos.java
@@ -0,0 +1,19 @@
+package Juegos;
+public enum EnumJuegos {
+    Snake(0),
+    Packman(1),
+    Galaga(2),
+    Dinosaurio(3),
+    Blackjack(4),
+    Buscaminas(5);
+
+    private int valor;
+
+    private EnumJuegos(int valor){
+        this.valor = valor;
+    }
+
+    public int getValor(){
+        return valor;
+    }
+}
